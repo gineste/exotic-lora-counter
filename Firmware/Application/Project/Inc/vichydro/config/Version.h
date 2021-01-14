38aaf37cefda7bc395d27708faf9d5dd39917fa3
Marty <martin.cornu@exotic-systems.com>
2021-01-14 15:01:18 +0100

    take it-sdk 1.6 stable version commit

--- Firmware/Application/Project/Inc/vichydro/config/Version.h
+++ Firmware/Application/Project/Inc/vichydro/config/Version.h
@@ -25,7 +25,7 @@
 #define HW_VERSION   "1.A"
 
 #define BUILD_DATE   "2021-01-14"
-#define BUILD_TIME   "14:17:16"
+#define BUILD_TIME   "14:58:25"
 
 #define FW_VERSION_MAJOR	   (0x0)
 #define FW_VERSION_MINOR	   (0x2)
@@ -43,7 +43,7 @@
 #define ITSDK_USER_VERSION_BYTE		(FW_VERSION_MAJOR << 4) | FW_VERSION_MINOR
 
 /* COMMIT HASH */
-#define COMMIT_NUMBER    "3496cab"
+#define COMMIT_NUMBER    "32c0b35"
 
 /************************************************************************
  * Type definitions
